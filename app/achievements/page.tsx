22db82da443e891dbbef613ba162a78a408d904e
Muhammad Oka <oka@muhammadoka.dev>
2024-12-01 19:19:08 +0700

    feat: add new achievement

--- app/achievements/page.tsx
+++ app/achievements/page.tsx
@@ -5,7 +5,8 @@ export default function Achievements() {
         <div className="flex flex-col gap-4 transition-all duration-200">
           <h1 className="text-4xl sm:text-6xl text-cyan-500">achievements</h1>
           <ul className="list-disc list-inside">
-            <li>PwC Hackaday 2024 1st Runner Up (2024)</li>
+            <li>PwC Hack A Day 2024 1st Runner Up (2024)</li>
+            <li>Cyber Jawara International 2024 2nd Place (2024)</li>
             <li>CTF TCP1P International 2024 Winner (2024)</li>
             <li>Gemastik 2024 Keamanan Siber Finalist (2024)</li>
             <li>Technofair 11.0 CTF 3rd Place (2024)</li>
